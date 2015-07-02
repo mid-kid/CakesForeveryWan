e263c1ed7e59484ca78475dda2e02c659f8ecfc4
bilis <diefaust@gmail.com>
2015-07-03 00:47:45 +0800

    Rebase to latest Cakes

--- patches/dhs/patches.s
+++ patches/dhs/patches.s
@@ -1,12 +1,10 @@
 .nds
 
-.create "patch1.bin", 0x08086140
-.org 0x08086140
+.create "patch1.bin"
 .arm
 ldr pc, =0x01FF8000
 .close
 
-.create "patch2.bin", 0x08086174
-.org 0x08086174
+.create "patch2.bin"
 .pool
 .close
