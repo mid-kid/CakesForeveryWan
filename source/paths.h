02b94f7531acb383676b7cdfb84264c05a26bc2d
Alexander Hartmann <hartmannaf@googlemail.com>
2016-01-13 15:35:23 +0100

    add support to use slot0x11key9.6 and to load the newest firm on n3ds

--- source/paths.h
+++ source/paths.h
@@ -10,5 +10,6 @@
 #define PATH_FIRMKEY PATH_CAKES "/firmkey.bin"
 #define PATH_CETK PATH_CAKES "/cetk"
 #define PATH_SLOT0X25KEYX "/slot0x25keyX.bin"
+#define PATH_SLOT0X11KEY96 "/slot0x11key9.6.bin"
 #define PATH_PATCHES PATH_CAKES "/patches"
 #define PATH_CONFIG PATH_CAKES "/config.dat"
